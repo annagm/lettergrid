a2435cef2800c528f6bbd78bb4ff61ede3670301
annagm <60825109+annagm@users.noreply.github.com>
2020-05-15 13:38:06 -0400

    Update letterGrid.js

--- letterGrid.js
+++ letterGrid.js
@@ -6,6 +6,11 @@ var cubes = ["AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS",
 
 //building and displaying grid
 function rollBoard(grid){
+  var rotate = document.getElementById("Esther");
+  if (rotate.checked == true) {
+    rotateBoard(grid);
+  }
+  else {
   var board = document.getElementById("board");
 for(i=0; i<grid.length; i++) {
   var letter = document.createElement("DIV");
@@ -19,12 +24,43 @@ for(i=0; i<grid.length; i++) {
   board.appendChild(letter);
   }
 }
+}
+
+function rotateBoard(grid) {
+  var board = document.getElementById("board");
+
+for(i=0; i<grid.length; i++) {
+  var r = Math.floor(Math.random() * 9);
+  console.log(r);
+var rotation = "rotate(0deg)";
+  if (r == 9) {
+    rotation = "rotate(270deg)";
+  }
+  else if (r == 8) {
+    rotation = "rotate(180deg)";
+  }
+  else if (r == 7) {
+    rotation = "rotate(90deg)";
+  }
+
+  var letter = document.createElement("DIV");
+  letter.className = "grid-item";
+  if(grid[i] == "Q") {
+    letter.innerHTML = "Qu";
+  }
+  else {
+  letter.innerHTML = grid[i];
+  }
+  letter.style.transform = rotation;
+  board.appendChild(letter);
+  }
+}
 
 function shuffle(){
 var grid = new Array();
 while(cubes.length > 0){
   var i = Math.floor(Math.random() * (cubes.length-1));
-  var j = Math.floor(Math.random() * 5)
+  var j = Math.floor(Math.random() * 5);
   grid.push(cubes[i].charAt(j));
   cubes.splice(i , 1);
 }
